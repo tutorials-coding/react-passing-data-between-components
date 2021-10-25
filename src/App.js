9b661539f48737ce42cd4ead07e8f8b9ea3b0f01
Vadim <korobeinikovv@mail.ru>
2021-10-25 18:55:28 +0300

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-// import { Component } from './parent-to-child/Component'
-// import { Component } from './child-to-parent/Component'
-// import { Component } from './sibling-to-sibling/Component'
-import { Component } from './parent-to-grandchild/Component'
+import { Component as ParentToChild } from './parent-to-child/Component'
+import { Component as ChildToParent } from './child-to-parent/Component'
+import { Component as SiblingToSibling } from './sibling-to-sibling/Component'
+import { Component as ParentToGrandchild } from './parent-to-grandchild/Component'
 import './App.css'
 
 export default function App() {
   return (
-    <div className="App">
-      <Component />
+    <div className="app-container">
+      <div className="example-container">
+        <p>Parent to Child</p>
+        <ParentToChild />
+      </div>
+      <div className="example-container">
+        <p>Child to Parent</p>
+        <ChildToParent />
+      </div>
+      <div className="example-container">
+        <p>Sibling to Sibling</p>
+        <SiblingToSibling />
+      </div>
+      <div className="example-container">
+        <p>Parent to Grandchild</p>
+        <ParentToGrandchild />
+      </div>
     </div>
   )
 }
